31b31e72fac29d0bc95c631f2e6e30da5956be38
manish5258 <149806423+manish5258@users.noreply.github.com>
2023-11-04 12:39:10 +0530

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -16,7 +16,10 @@ const Contact = () => {
                     <ContactForm />
                 </section>
             </div>
-
+                <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14005.603744305567!2d77.41816918654543!3d28.6477114415572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf02cab9e16df%3A0xb03482d8c00e67c!2sVijay%20Nagar%2C%20Ghaziabad%2C%20Uttar%20Pradesh%20201009!5e0!3m2!1sen!2sin!4v1699004876531!5m2!1sen!2sin"
+                width={100} height={450} style={{border: 0}} allowFullScreen="" loading="lazy" className={styles.mapping}
+                referrerPolicy="no-referrer-when-downgrade"> </iframe>
             
 
         </>
