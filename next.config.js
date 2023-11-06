efe24715d2122e0e709832b0201ae726fe5c0234
manish5258 <149806423+manish5258@users.noreply.github.com>
2023-11-06 17:24:33 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@ const nextConfig = {
     experimental: {
       appDir: true,
       serverComponentsExternalPackages: ["mongoose"],
+      serverActions:true,
     },
     images:{
-      domains:['occ-0-3933-116.1.nflxso.net']
+      domains:['occ-0-3933-116.1.nflxso.net']  
     }
   }
   
